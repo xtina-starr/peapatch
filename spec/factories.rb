2b8ad2d758f5b7d74307875e0b6a05c282cd9c7d
Christina Thompson <c.thompsonms@gmail.com>
2014-02-11 14:38:51 -0800

    created a factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,4 +5,15 @@ FactoryGirl.define do
     datetime Time.now
     user_id 1
   end
+
+  factory :user do
+    uid '12345677'
+    provider 'Twitter'
+    avatar_url 'thisisaurl.com'
+    username 'MrCool'
+    email 'mrcool@cool.com'
+    admin 'false'
+    access_token 'somenumbers12345'
+    access_token_secret 'shhhitsasecret12345'
+  end
 end
