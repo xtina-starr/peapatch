a7276f3082c1aff67e10d9563704283b9229d290
Christina Thompson <c.thompsonms@gmail.com>
2014-02-14 15:53:04 -0800

    fix form params

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,7 +20,7 @@ class SessionsController < ApplicationController
   end
 
   def user_email
-    @current_user.update(email: params[:email][:email])
+    @current_user.update(email: params[:email])
   
       redirect_to profile_path, notice: "Thank you for submitting your email address."
   end
