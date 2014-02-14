586e491b8160aa689dd5bb5c96498540fcc069db
Christina Thompson <c.thompsonms@gmail.com>
2014-02-14 13:01:10 -0800

    wrote spec to test user_email

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 describe SessionsController do
+
+  describe 'GET index' do
+    it 'is successful' do
+      get :index
+      expect(response).to be_successful
+    end
+  end
+
   describe 'user email method' do
     let(:current_user) { create(:user) }
     before(:each) do
@@ -7,8 +15,8 @@ describe SessionsController do
       end
 
     it 'updates user email address' do
-      post :user_email, id: current_user.id, email: current_user.email
-      expect(current_user.email).to_not be_nil
+      post :user_email, { id: current_user.id, email: current_user.email}
+      expect(current_user.reload.email).to_not be_nil
     end
   end
   
