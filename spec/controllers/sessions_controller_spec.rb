5d05e2fe2fb3f7c93adeee717991e0c24bd563e2
Christina Thompson <c.thompsonms@gmail.com>
2014-02-19 11:21:05 -0800

    moved user_email spec to user controller spec

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -8,16 +8,16 @@ describe SessionsController do
     end
   end
 
-  describe 'user email method' do
-    let(:current_user) { create(:user) }
-    before(:each) do
-        controller.instance_variable_set(:@current_user, current_user)
-      end
+  # describe 'user email method' do
+  #   let(:current_user) { create(:user) }
+  #   before(:each) do
+  #       controller.instance_variable_set(:@current_user, current_user)
+  #     end
 
-    it 'updates user email address' do
-      post :user_email, { id: current_user.id, email: current_user.email}
-      expect(current_user.reload.email).to_not be_nil
-    end
-  end
+  #   it 'updates user email address' do
+  #     post :user_email, { id: current_user.id, email: current_user.email}
+  #     expect(current_user.reload.email).to_not be_nil
+  #   end
+  # end
   
 end
