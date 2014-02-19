5e467175e0ae11b395a88cb030a1df6157dbd69a
shanghaiellen@gmail.com <shanghaiellen@gmail.com>
2014-02-19 09:39:52 -0800

    adds signout button and only permits logged-in users to post to/access calendar

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,11 +1,20 @@
 class CalendarsController < ApplicationController
   def index
     @event = Event.new
+    if @current_user.nil?
+      redirect_to '/', notice: 'You must be a logged-in user to do that'
+    elsif @current_user.email.nil?
+      redirect_to profile_path, notice: 'You need to complete your profile to do that'
+    end
     # Event.new_event({title: 'a title', start_time: Time.now, end_time: Time.now + (60*60)})
   end
 
   def create
-    Event.new_event(params)
-    redirect_to :back
+    if Event.valid(params)
+      Event.new_event(params)
+      redirect_to :back
+    else
+      redirect_to :back, notice: 'That is not a valid event!'
+    end
   end
 end
