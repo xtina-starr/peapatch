5744e43e297b1bec182e7e8c7475f540e32abfcb
Christina Thompson <c.thompsonms@gmail.com>
2014-02-19 11:22:30 -0800

    added user_email spec

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+describe UsersController do
+
+  describe 'user email method' do
+    let(:current_user) { create(:user) }
+    before(:each) do
+        controller.instance_variable_set(:@current_user, current_user)
+      end
+
+    it 'updates user email address' do
+      post :user_email, { id: current_user.id, email: current_user.email}
+      expect(current_user.reload.email).to_not be_nil
+    end
+  end
+end
