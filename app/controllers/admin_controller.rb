79d88a93664386f408f85721780ec3e199d12699
Christina Thompson <c.thompsonms@gmail.com>
2014-02-19 15:41:52 -0800

    made current_user an instance method

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -7,7 +7,6 @@ class AdminController < ApplicationController
 
   def admin_access
     @user = User.find(params[:id])
-
     @user.update(admin: true)
     redirect_to admin_path, notice: "You have successfully added #{@user.username} as an Admin User."
 
@@ -15,14 +14,13 @@ class AdminController < ApplicationController
 
   def revoke_admin
     @user = User.find(params[:id])
-
     @user.update(admin: false)
     redirect_to admin_path, notice: "You have successfully revoked #{@user.username}'s admin access."
   end
 
   private
   def check_admin
-    unless current_user.admin?
+    unless @current_user.admin?
       redirect_to root_path, notice: "You do not have Admin access!"
     end
   end
