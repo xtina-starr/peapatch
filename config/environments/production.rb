26cead88f3ffd35f3e9a86b2bdd9a41456a489a8
shanghaiellen@gmail.com <shanghaiellen@gmail.com>
2014-02-21 14:17:34 -0800

    adds more configs to mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,9 @@ Peapatch::Application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'adapeapatch.herokuapp.com'}
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
