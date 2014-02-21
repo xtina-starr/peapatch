93cbad6a8690dfd04dedde7993cd6eb9acbd086e
shanghaiellen@gmail.com <shanghaiellen@gmail.com>
2014-02-21 15:33:14 -0800

    returns to instance method

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -1,7 +1,7 @@
 class PostMailer < ActionMailer::Base
   default from: "adapeapatch@gmail.com"
 
-  def self.new_post(post_id)
+  def new_post(post_id)
     @post = Post.find(post_id)
     User.all.each do |user|
       mail(to: user.email, subject: "New post: #{@post.title}")
